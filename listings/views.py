9344ffbe457c43fe91f89545f876104ca4b2f662
OGustavo-Silva <gustavos_barros@hotmail.com>
2020-11-09 19:04:57 -0300

    search implementado

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
 
 from .models import Listing
 
@@ -14,7 +15,49 @@ def index(request):
     return render(request, 'listings/listings.html', context)
     
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html' )
+    listing = get_object_or_404(Listing,pk=listing_id)
+
+    context = {'listing': listing}
+    return render(request, 'listings/listing.html', context)
     
 def search(request):
-    return render(request, 'listings/search.html' )
+    query_set = Listing.objects.order_by('-list_date')
+
+    #Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_set = query_set.filter(description__icontains=keywords)
+
+    #City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_set = query_set.filter(city__iexact=city)
+
+    #State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_set = query_set.filter(state__iexact=state)
+
+    #Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_set = query_set.filter(bedrooms__lte=bedrooms)
+
+    #Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query_set = query_set.filter(price__lte=price)
+
+    context = {
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'state_choices': state_choices,
+        'listings': query_set
+    }
+
+    return render(request, 'listings/search.html', context)
